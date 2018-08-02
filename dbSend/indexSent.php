a1c16c8e70c2a086db0359c90e4c23fc94691f65
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-08-02 16:47:18 +0200

    .

--- dbSend/indexSent.php
+++ dbSend/indexSent.php
@@ -9,7 +9,7 @@ $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('could not
 
 mysqli_set_charset($dbc, "utf8"); */
 
-require 'connection/connect.php';
+require '../connection/connect.php';
 
 $firstSign = $_REQUEST['firstSign'];
 $lastSign = $_REQUEST['lastSign'];
