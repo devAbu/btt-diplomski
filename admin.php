11ed5804ed673ba747e647926dbd8e861417733f
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-08-29 22:22:36 +0200

    count tours and users

--- admin.php
+++ admin.php
@@ -17,14 +17,11 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-  <!-- <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all"> -->
   <link href="css/style2.css" rel="stylesheet" type="text/css" media="all" />
-  <link href="css/font-awesome.css" rel="stylesheet">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
       crossorigin="anonymous"></script>
@@ -39,6 +36,11 @@
 </head>
 
 <body>
+
+  <?php
+    require '/connection/connect.php';
+?>
+
   <div class="page-container">
     <div class="left-content">
       <div class="mother-grid-inner">
@@ -60,7 +62,13 @@
                 <div class="market-update-block clr-block-1">
                   <div class="row">
                     <div class="col-md-8 market-update-left">
-                      <h3>83</h3>
+                      <?php
+                          $sql = "SELECT * FROM registacija ";
+                          $result = $dbc->query($sql);
+
+                          $count = $result->num_rows;
+                          echo '<h3>'.$count.'</h3> ';
+                        ?>
                       <h4>Registered Users</h4>
                     </div>
                     <div class="col-md-4 market-update-right">
@@ -74,7 +82,13 @@
                 <div class="market-update-block clr-block-2">
                   <div class="row">
                     <div class="col-md-8 market-update-left">
-                      <h3>135</h3>
+                      <?php
+                          $sql = "SELECT * FROM tourplan ";
+                          $result = $dbc->query($sql);
+
+                          $count = $result->num_rows;
+                          echo '<h3>'.$count.'</h3> ';
+                        ?>
                       <h4>Tours</h4>
                     </div>
                     <div class="col-md-4 market-update-right">
@@ -224,7 +238,7 @@
     <div class="sidebar-menu">
       <div class="menu">
         <ul id="menu">
-          <li id="menu-home"><a href="logout.php"><i class="fa fa-lg fa-sign-out"></i><span>Logout</span></a></li>
+          <li id="menu-home"><a href="logout.php"><i class="fas fa-sign-out-alt fa-lg mr-2"></i><span>Logout</span></a></li>
         </ul>
       </div>
     </div>
